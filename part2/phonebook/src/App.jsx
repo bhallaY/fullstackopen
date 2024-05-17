ff7dd2d686bb4c93da35db5faac3ac74f719abe4
Yash Bhalla <bhalla.yash98@gmail.com>
2024-05-17 12:22:14 -0700

    Display persons in phonebook, add phonebook and alerts for duplicate entries

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,38 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.filter((person) => person.name === newName).length > 0) {
+      alert(`${newName} is already added to phonebook`)
+    }
+
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input id='Add-Name' value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
   )
 }
