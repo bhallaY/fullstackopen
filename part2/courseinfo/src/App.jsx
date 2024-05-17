54a6d75f00b1c82305059e6512575816cbb9092f
Yash Bhalla <bhalla.yash98@gmail.com>
2024-05-17 10:55:35 -0700

    Refactor Components to component module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,32 +1,6 @@
+import Course from "./components/Course"
+
 /* eslint-disable react/prop-types */
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map((part) => <Part key={part.id} part={part} />)}
-  </>
-
-const Course = ({ course }) => {
-  const sumOfExercises = course.parts.reduce(
-    (countedExercises, part) => countedExercises + part.exercises,
-    0)
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={sumOfExercises} />
-    </div>
-  )
-}
-
 const App = () => {
   const courses = [
     {
