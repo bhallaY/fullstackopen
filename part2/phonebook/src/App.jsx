de36ccdbda7f9d4654886a98ceb89d33c2f6adbf
Yash Bhalla <bhalla.yash98@gmail.com>
2024-05-17 12:33:44 -0700

    Add test data, block duplicate entries, associate names with numbers

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,25 +2,37 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.filter((person) => person.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`)
+      return
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -31,12 +43,15 @@ const App = () => {
           name: <input id='Add-Name' value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input id='Add-Number' value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+        {persons.map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
